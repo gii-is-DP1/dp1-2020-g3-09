e386598a39867bf9125383f646e8266bb1248797
rodgarcas <rodgarcas@alum.us.es>
2020-11-11 19:31:43 +0100

    Estructura básica de directorios y POM.xml predefinido

---
+++ src/main/java/com/tempura17/Tempura17Application.java
@@ -0,0 +1,13 @@
+package com.tempura17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tempura17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tempura17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tempura17/Tempura17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tempura17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tempura17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
