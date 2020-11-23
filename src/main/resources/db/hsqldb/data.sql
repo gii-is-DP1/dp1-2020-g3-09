32e0ba077a44dae85f87f6e37c77a17a6f2b5588
manguecor <manguecor@alum.us.es>
2020-11-23 19:37:14 +0100

    Actualizo mi data.sql con una inserción de calculadora

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -1,3 +1,6 @@
 
 INSERT INTO pacientes(id, first_name, last_name, dni, email, direccion, sexo, edad) VALUES (1, 'Rodrigo', 'García', '00000000A', 'cosas@gmail.com', 'micasa', 'MASCULINO', 20);
 INSERT INTO pacientes(id, first_name, last_name, dni, email, direccion, sexo, edad) VALUES (2, 'Antonio', 'Fernández', '00000000B','cosas1@gmail.com', 'sucasa', 'FEMENINO', 20);
+
+
+INSERT INTO calculadora(id, peso, altura, imc) VALUES (1, 67.5, 1.84, 20);
