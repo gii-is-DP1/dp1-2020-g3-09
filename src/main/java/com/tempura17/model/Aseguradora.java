db3df1907d1d96152afcf42dd94943b86d0b6986
antonio <antferdom@alum.us.es>
2020-11-24 13:39:15 +0100

    Issue #16: sincronizada adiciones de Rodgarcas

---
+++ src/main/java/com/tempura17/model/Aseguradora.java
@@ -0,0 +1,50 @@
+package com.tempura17.model;
+
+/*
+import java.util.Set;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import org.springframework.beans.support.MutableSortDefinition;
+import org.springframework.beans.support.PropertyComparator;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Data;
+*/
+//@Entity
+//@Data
+//@Table(name = "aseguradoras")
+public class Aseguradora extends BaseEntity{
+
+    /*
+    NOTA: Necesita discutir y aobrdar detalles de implementación severos.
+
+    private String nombre;
+
+   @OneToMany(cascade = CascadeType.ALL, mappedBy = "aseguradora", fetch = FetchType.EAGER)
+   private Set<Especialista> especialistaes;
+
+   @OneToMany(cascade = CascadeType.ALL, mappedBy = "aseguradora", fetch = FetchType.EAGER)
+   private Set<Paciente> pacientes;
+    */
+    
+}
