3a76736d6f92d4ca81af9abf2b1b0cd7feafb55c
manguecor <manguecor@alum.us.es>
2020-11-28 11:36:08 +0100

    Issue #22 Cambios en el repository

--- src/main/java/com/tempura17/repository/CalculadoraRepository.java
+++ src/main/java/com/tempura17/repository/CalculadoraRepository.java
@@ -2,7 +2,10 @@ package com.tempura17.repository;
 
 import java.util.Collection;
 
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import com.tempura17.model.CalculadoraSalud;
 
 public interface CalculadoraRepository extends CrudRepository<CalculadoraSalud,Long>{
@@ -10,5 +13,8 @@ public interface CalculadoraRepository extends CrudRepository<CalculadoraSalud,L
     Collection<CalculadoraSalud> findAll();
 
     CalculadoraSalud findById(long id);
+
+    @Query("SELECT calc FROM CalculadoraSalud calc WHERE calc.paciente.id = :pacienteId")
+    Collection<CalculadoraSalud> findByPacienteId(@Param("pacienteId")int pacienteId);
 
 }
