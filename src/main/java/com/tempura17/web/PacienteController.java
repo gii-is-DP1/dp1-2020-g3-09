d27d6369e92389190fc9583a8a7bc4f5a351a6d4
manguecor <manguecor@alum.us.es>
2020-11-28 11:41:48 +0100

    Issue #22 Cambios en el controller del paciente introduciendo el obtener calculadora por id del paciente

--- src/main/java/com/tempura17/web/PacienteController.java
+++ src/main/java/com/tempura17/web/PacienteController.java
@@ -9,7 +9,11 @@ import javax.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.tempura17.model.Paciente;
+import com.tempura17.model.CalculadoraSalud;
+import com.tempura17.model.Cita;
 import com.tempura17.service.PacienteService;
+import com.tempura17.service.CalculadoraService;
+import com.tempura17.service.CitaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -26,7 +30,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class PacienteController {
 
     @Autowired
-    PacienteService pacienteServ;
+	PacienteService pacienteServ;
+	
+	@Autowired
+	CalculadoraService calculadoraServ;
+
+	@Autowired
+	CitaService citaServ;
     
     @GetMapping
 	public String listPacientes(ModelMap model)
@@ -42,5 +52,29 @@ public class PacienteController {
 		
 		return pacienteServ.findAll();
 	}
+
+	@GetMapping(value="/{pacienteId}/calculadoras")
+	public String getPacienteCalculadora(@PathVariable("pacienteId") int pacienteId, ModelMap model){
+		model.addAttribute("calculadoras", calculadoraServ.findByPacienteId(pacienteId));
+		return "calculadoras/calculadoraListing";
+	}
+
+	@GetMapping(value="/{pacienteId}/calculadoras/json")
+	@ResponseBody
+	public Collection<CalculadoraSalud> getPacienteCalculadoraJson(@PathVariable("pacienteId") int pacienteId, ModelMap model){
+		return calculadoraServ.findByPacienteId(pacienteId);
+	}
+	
+	/*@GetMapping(value="/{pacienteId}/citas")
+	public String getPacienteCitas(@PathVariable("pacienteId") int pacienteId, ModelMap model){
+		model.addAttribute("citas", citaServ.findByPacienteId(pacienteId));
+		return "citas/History";
+	}
+
+	@GetMapping(value="/{pacienteId}/citas/json")
+	@ResponseBody
+	public Collection<Cita> getPacienteCitasJson(@PathVariable("pacienteId") int pacienteId, ModelMap model){
+		return citaServ.findByPacienteId(pacienteId);
+	}*/
     
 }
