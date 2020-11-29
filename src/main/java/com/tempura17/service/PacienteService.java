b4a198bff7e9725c6676f41d398fa6f68b6852f0
manguecor <manguecor@alum.us.es>
2020-11-29 16:26:04 +0100

    Issue #22 Cambios en el service para obtener calculadora por paciente id

--- src/main/java/com/tempura17/service/PacienteService.java
+++ src/main/java/com/tempura17/service/PacienteService.java
@@ -6,7 +6,10 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tempura17.model.CalculadoraSalud;
 import com.tempura17.model.Paciente;
+import com.tempura17.repository.CalculadoraRepository;
 import com.tempura17.repository.PacienteRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +18,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class PacienteService {
 
+    private PacienteRepository repo;
+
+    private CalculadoraRepository calculadoraRepository;
+
     @Autowired
-    PacienteRepository repo;
+	  public PacienteService(PacienteRepository repo,
+			CalculadoraRepository calculadoraRepository) {
+		this.repo = repo;
+		this.calculadoraRepository = calculadoraRepository;
+	}
     
     public Collection<Paciente> findAll(){
-      
-		return repo.findAll();
-	}
+    return repo.findAll();
+    }
+    
+    public CalculadoraSalud findCalculadoraByPacienteId(int pacienteId){
+      return calculadoraRepository.findByPacienteId(pacienteId);
+    }
+
 	
     
 }
