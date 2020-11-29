90526cb8faeedb43a41ea283af6f82b03fa4d9f1
manguecor <manguecor@alum.us.es>
2020-11-29 16:27:46 +0100

    Issue #22 Pruebas del service de calculadora [estable]

--- src/test/java/com/tempura17/service/CalculadoraServiceTests.java
+++ src/test/java/com/tempura17/service/CalculadoraServiceTests.java
@@ -1,40 +1,41 @@
-/*package com.tempura17.service;
+package com.tempura17.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Collection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.dao.DataAccessException;
+
 
 import com.tempura17.model.CalculadoraSalud;
-import com.tempura17.util.EntityUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
-class CitaServiceTests {
+public class CalculadoraServiceTests {
 
     @Autowired
     private CalculadoraService calculadoraServ;
 
+    @Autowired
+    private PacienteService pacienteServ;
+
+
 
     @Test
+
 	@Transactional
-	public void shouldInsertCalculadora() {
+	void shouldInsertCalculadora() {
 		Collection<CalculadoraSalud> calculadoras = this.calculadoraServ.findAll();
 		int found = calculadoras.size();
 
 		CalculadoraSalud calculadora = new CalculadoraSalud();
-        calculadora.setPeso(75.3);
-        calculadora.setAltura(1.84);
+        calculadora.setPeso("75.3");
+        calculadora.setAltura("1.84");
         calculadora.setimc(27.54);
         calculadora.setResultado("Peso normal");              
                 
@@ -43,6 +44,15 @@ class CitaServiceTests {
     }
     
 
-    //Me faltará hacer el test del findByPacienteId
+    @Test
+	void shouldFindCalculadoraByPacienteId() throws Exception {
+        CalculadoraSalud calculadora = this.pacienteServ.findCalculadoraByPacienteId(1);
+        assertThat(calculadora.getId()).isEqualTo(1);
+		assertThat(calculadora.getPeso()).isNotNull();
+		assertThat(calculadora.getAltura()).isNotNull();
+    }
     
-}*/
+
+    
+    
+}
