c754069ee19f291000919b6376ff7fa53701b846
manguecor <manguecor@alum.us.es>
2020-12-09 11:13:41 +0100

    Issue #19 Creación del service de alarma

---
+++ src/main/java/com/tempura17/service/AlarmaService.java
@@ -0,0 +1,49 @@
+package com.tempura17.service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tempura17.model.Alarma;
+import com.tempura17.repository.AlarmaRepository;
+import com.tempura17.repository.CitaRepository;
+
+@Service
+public class AlarmaService {
+	
+	private AlarmaRepository alarmaRepository;
+	
+	@Autowired
+	public AlarmaService(AlarmaRepository alarmaRepository, CitaRepository citaRepository) {
+		super();
+		this.alarmaRepository = alarmaRepository;
+	}
+	
+	public Collection<Alarma> findAll(){
+		return alarmaRepository.findAll();
+	}
+	
+	public Optional<Alarma> findById(int id){
+		return alarmaRepository.findById(id); 
+	}
+	
+	public Alarma findByCitaId(Integer citaId) {
+		return alarmaRepository.findByCitaId(citaId);
+	}
+	
+	public void save(@Valid Alarma alarma) {
+		alarmaRepository.save(alarma);
+	}
+	
+	public void delete(Alarma alarma) {
+		alarmaRepository.delete(alarma);
+	}
+	
+	//Crear mensaje cuando fecha local = cita.fecha - diasAntelacion
+	
+
+}
