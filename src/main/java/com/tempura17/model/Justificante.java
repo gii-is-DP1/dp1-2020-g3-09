93a7734b04fb5e429e60763ea211c0a722d39f1d
manguecor <manguecor@alum.us.es>
2021-01-04 15:28:03 +0100

    Issue #20 Modificacion del model de justificante

--- src/main/java/com/tempura17/model/Justificante.java
+++ src/main/java/com/tempura17/model/Justificante.java
@@ -1,27 +1,16 @@
 package com.tempura17.model;
 
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import javax.persistence.JoinTable;
+
 
 
 @Entity
@@ -51,6 +40,6 @@ public class Justificante extends BaseEntity{
     }
 
     public void setMotivo(String motivo){
-        this.motivo=motivo;
+        this.motivo = motivo;
     }
 }
