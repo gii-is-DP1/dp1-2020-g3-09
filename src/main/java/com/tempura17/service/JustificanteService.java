df68c6b3e2888a017e5cbd85f6ff931419343f1d
manguecor <manguecor@alum.us.es>
2021-01-04 15:28:37 +0100

    Issue #20 Modificacion del service de justificante

--- src/main/java/com/tempura17/service/JustificanteService.java
+++ src/main/java/com/tempura17/service/JustificanteService.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Service;
 import java.util.Collection;
 import java.util.Optional;
 
-import javax.transaction.Transactional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 
+
+import com.tempura17.model.Cita;
 import com.tempura17.model.Justificante;
 import com.tempura17.repository.JustificanteRepository;
 
@@ -20,9 +21,12 @@ public class JustificanteService {
 
     private JustificanteRepository justificanteRepository;
 
+    private CitaService citaService;
+
     @Autowired
-    public JustificanteService(JustificanteRepository justificanteRepository){
+    public JustificanteService(JustificanteRepository justificanteRepository, CitaService citaService){
         this.justificanteRepository = justificanteRepository;
+        this.citaService=citaService;
     }
 
 
@@ -41,7 +45,11 @@ public class JustificanteService {
     public void save(@Valid Justificante justificante){
         justificanteRepository.save(justificante);
       }
+
   
-      
-    
+    public void createJustificanteForCitaId(Justificante justificante, Integer cita_id){
+       Optional<Cita> cita = this.citaService.findById(cita_id);
+       justificante.setCita(cita.get());
+       justificanteRepository.save(justificante);
+    }
 }
