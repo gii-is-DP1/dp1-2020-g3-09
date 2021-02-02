ecc9006ca96377292b36256615dd9e7e649de62f
rodgarcas <rodgarcas@alum.us.es>
2021-02-02 14:56:58 +0100

    (MODIFICACION) PacienteService -> CalculadoraRepository: CalculadoraService

--- src/main/java/com/tempura17/service/PacienteService.java
+++ src/main/java/com/tempura17/service/PacienteService.java
@@ -12,7 +12,7 @@ import org.springframework.dao.DataAccessException;
 
 import com.tempura17.model.CalculadoraSalud;
 import com.tempura17.model.Paciente;
-import com.tempura17.repository.CalculadoraRepository;
+import com.tempura17.service.CalculadoraService;
 import com.tempura17.repository.PacienteRepository;
 import org.springframework.stereotype.Service;
 
@@ -22,16 +22,16 @@ public class PacienteService {
 
     PacienteRepository pacienteRepository;
 
-    CalculadoraRepository calculadoraRepository;
+    CalculadoraService calculadoraService;
 
     UserService userService;
 
     AuthoritiesService authoritiesService;
     
     @Autowired
-    public PacienteService(PacienteRepository pacienteRepository,CalculadoraRepository calculadoraRepository,UserService userService,AuthoritiesService authoritiesService){
+    public PacienteService(PacienteRepository pacienteRepository,CalculadoraService calculadoraService ,UserService userService,AuthoritiesService authoritiesService){
       this.pacienteRepository = pacienteRepository;
-      this.calculadoraRepository = calculadoraRepository;
+      this.calculadoraService = calculadoraService;
       this.userService = userService;
       this.authoritiesService = authoritiesService;
     }
@@ -63,7 +63,7 @@ public class PacienteService {
 
     @Transactional
     public CalculadoraSalud findCalculadoraByPacienteId(int pacienteId){
-      return calculadoraRepository.findByPacienteId(pacienteId);
+      return calculadoraService.findByPacienteId(pacienteId);
     }
 
 
